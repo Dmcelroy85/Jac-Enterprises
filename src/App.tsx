85847a7fc546f51104de240fc5a356e718401643
Musaf95 <133581173+Musaf95@users.noreply.github.com>
2023-09-07 20:55:26 -0400

    update git

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+}
+
+export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+interface Props {
+    items: string[];
+    title: string;
+    onItemClick: (item: string) => void;
+}
+
+function ListGroup(props: Props) {
+
+}
+
+export default ListGroup;
