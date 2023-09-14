e079ca78efb48bc3f12a7e16bd88a23707fc27ee
Musaf95 <133581173+Musaf95@users.noreply.github.com>
2023-09-14 01:16:15 -0400

    Fixing code

--- src/App.tsx
+++ src/App.tsx
@@ -8,44 +8,52 @@ import './App.css';
 function App() {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
-    const [selectedImage, setSelectedImage] = useState(null); 
-    const [isLoading, setIsLoading] = useState(false); 
-    const [isSearching, setIsSearching] = useState(false); 
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isSearching, setIsSearching] = useState(false);
+    const [hasError, setHasError] = useState(false);
 
     const API_URL = 'https://images-api.nasa.gov/search';
 
     useEffect(() => {
-        
         const searchImages = async () => {
-            setIsSearching(true); 
-            setIsLoading(true); 
+            setIsSearching(true); // Set isSearching to true when starting the search
+            setIsLoading(true); // Set isLoading to true when starting the search
+
             try {
                 const response = await fetch(`${API_URL}?q=${searchTerm}`);
                 const data = await response.json();
-                console.log(data)
-                if (JSON.stringify(response)==="{}"){
-                setSearchResults(data.collection.items);}
+                
+                if (data.collection && data.collection.items) {
+                    setSearchResults(data.collection.items); // Update searchResults with fetched data
+                } else {
+                    setSearchResults([]); // Set results to empty if there are no items
+                }
             } catch (error) {
                 console.error('Error:', error);
+                setHasError(true); // Set hasError to true if there's an error
             } finally {
-                setIsSearching(false); 
-                setIsLoading(false); 
+                setIsSearching(false); // Set isSearching to false when search is complete
+                setIsLoading(false); // Set isLoading to false when search is complete
             }
         };
 
         if (searchTerm) {
             searchImages();
+        } else {
+            setSearchResults([]); // Clear searchResults when search term is empty
+            setHasError(false); // Reset hasError when search term is empty
         }
     }, [searchTerm]);
 
     const showImageDetails = (image) => {
-        setSelectedImage(image);
+        setSelectedImage(image); // Set selectedImage to the clicked image
     };
 
     const clearSearch = () => {
-        setSearchTerm('');
-        setSearchResults([]);
-        setSelectedImage(null);
+        setSearchTerm(''); // Clear the search term
+        setSearchResults([]); // Clear search results
+        setSelectedImage(null); // Clear selected image
     };
 
     const scrollToTop = () => {
@@ -64,29 +72,43 @@ function App() {
                     className={`nasa-logo ${isSearching ? 'large' : 'small'}`}
                 />
                 <h1>Mystery Educator</h1>
-                <input
-                    type="text"
-                    placeholder="Search NASA Images"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    className={`custom-search-input ${isSearching ? 'searching' : ''}`}
-                />
-                {isLoading && <LoadingIndicator />}
-                <button onClick={clearSearch}>Clear Search</button>
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="Search NASA Images"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className={`custom-search-input ${isSearching ? 'searching' : ''}`}
+                    />
+                    {isLoading && <LoadingIndicator />} {/* Display loading indicator while isLoading is true */}
+                </div>
+                <button onClick={clearSearch} className="clear-button">
+                    Clear Search
+                </button>
                 <button className="scroll-button" onClick={scrollToTop}>
                     Scroll to Top
                 </button>
                 <div className="image-results">
-                    {searchResults.map((item) => (
-                        <div key={item.data[0].nasa_id} onClick={() => showImageDetails(item)}>
-                            <img src={item.links[0].href} alt={item.data[0].title} />
-                            <p>{item.data[0].title}</p>
-                        </div>
-                    ))}
+                    {hasError ? (
+                        <p>An error occurred. Please try again later.</p>
+                    ) : searchResults.length > 0 ? (
+                        searchResults.map((item) => (
+                            <div key={item.data?.[0]?.nasa_id} onClick={() => showImageDetails(item)}>
+                                <img src={item.links?.[0]?.href} alt={item.data?.[0]?.title} />
+                                <p>{item.data?.[0]?.title}</p>
+                            </div>
+                        ))
+                    ) : (
+                        isSearching ? (
+                            <p>Loading...</p>
+                        ) : (
+                            <p>No results found.</p>
+                        )
+                    )}
                 </div>
             </header>
             <footer className="App-footer">
-                © Jac Enterprises 2023
+                © 2023 Jac Enterprises 
             </footer>
         </div>
     );
--- src/components/ImageDetails.tsx
+++ src/components/ImageDetails.tsx
@@ -3,32 +3,30 @@ import React from 'react';
 interface ImageDetailsProps {
     image: {
         data: {
-        title: string;
-        description: string;
+            title: string;
+            description: string;
         }[];
         links: {
-        href: string;
+            href: string;
         }[];
     };
-    onClose: () => void;
-    }
+}
 
-    const ImageDetails: React.FC<ImageDetailsProps> = ({ image }) => {
+const ImageDetails: React.FC<ImageDetailsProps> = ({ image }) => {
     return (
         <div className="card">
-        <div className="card-body">
-            <h5 className="card-title">{image.data[0].title}</h5>
-            <p className="card-text">{image.data[0].description}</p>
-            {}
-            <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
-        </div>
-        <img
-            src={image.links[0].href}
-            className="card-img-bottom"
-            alt={image.data[0].title}
-        />
+            <div className="card-body">
+                <h5 className="card-title">{image.data[0].title}</h5>
+                <p className="card-text">{image.data[0].description}</p>
+                <p className="card-text">
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </p>
+            </div>
+            <img
+                src={image.links[0].href}
+                className="card-img-bottom"
+                alt={image.data[0].title}
+            />
         </div>
     );
 };
