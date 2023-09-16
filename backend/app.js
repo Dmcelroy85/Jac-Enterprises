da50e84ced0d59b07c55b668bb3bd853c5a1d290
Musaf95 <133581173+Musaf95@users.noreply.github.com>
2023-09-15 20:15:01 -0400

    adding backend

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const team = require('./routes/team/team')
+const users = require('./routes/users/users')
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.use('/team', team)
+app.use('/users', users)
+app.get('/', (req, res) => {
+  res.send('Server is Alive')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
